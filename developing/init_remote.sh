cc363117dea34bdefc5abe89fb4549ce00039c72
Luo Chunhui <wo@luochunhui.com>
2019-01-15 10:54:44 +0800

    init

---
+++ developing/init_remote.sh
@@ -0,0 +1,21 @@
+git clone git@github.com:gikoluo/bitnami-docker-suitecrm.git bitnami-docker-suitecrm
+
+git clone https://github.com/nitmir/django-cas-server.git django-cas-server && cd django-cas-server && git checkout dev && cd - ;
+
+git clone git@github.com:gikoluo/django_oauth2_example.git django_oauth2_example
+
+git clone git@github.com:gikoluo/jenkins_example.git jenkins
+
+git clone git@github.com:gikoluo/jira.git jira
+
+git clone https://github.com/Kong/kong-dist-kubernetes.git kong-dist-kubernetes
+
+git clone git@github.com:gikoluo/minio.git minio
+
+git clone git@github.com:gikoluo/noty.git moty_flask
+
+git clone git@github.com:gikoluo/django_oauth2.git oauth2
+
+git clone git@github.com:gikoluo/yapi-docker.git yapi
+
+
